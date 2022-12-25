343de3ba778edaabbd211ef652673d9cbd8917b2
Amin Fadaeinejad <aminfadaei116@gmail.com>
2022-12-25 12:27:51 -0500

    changes

--- ProjectName/src/main/Functions.py
+++ ProjectName/src/main/Functions.py
@@ -1,7 +1,7 @@
 """
-DatabaseSetingsModule
+Functions Script
 ----------------------
-This is a Module which has Class for Database and database Settings
+This is the needed function for our process
 """
 
 import numpy as np
@@ -15,7 +15,13 @@ import cv2
 
 
 def createMask(keys, height, width, img=None):
-    
+    """
+    This will create a mask which only has value at the keypoint locations
+    :param keys: torch.tensor
+    :param height: int
+    :param width: int
+    :param img: torch.tensor
+    """
     if img != None:
       newImg = img.clone().detach().to(DEVICE)
       newImg[:, (height*keys[:, 1]).long(), (width*keys[:, 0]).long()] = 255.0
@@ -28,23 +34,16 @@ def createMask(keys, height, width, img=None):
 
 
 def TransformKeys(keys, euler, T):
+    """
+    This will create a mask which only has value at the keypoint locations
+    :param keys: torch.tensor
+    :param height: int
+    :param width: int
+    :param img: torch.tensor
+    """
     R = torch.linalg.multi_dot((Rx(euler[0]), Ry(euler[1]), Rz(euler[2])))
     center = torch.mean(keys, dim=0)
     keys = keys - center
     out = torch.matmul(keys, R)
     return out + center + T
     
-def Rx(theta):
-    return torch.tensor([[ 1, 0           , 0           ],
-                   [ 0, torch.cos(theta),-torch.sin(theta)],
-                   [ 0, torch.sin(theta), torch.cos(theta)]], device=DEVICE, dtype=torch.double)
-  
-def Ry(theta):
-    return torch.tensor([[ torch.cos(theta), 0, torch.sin(theta)],
-                   [ 0           , 1, 0           ],
-                   [-torch.sin(theta), 0, torch.cos(theta)]], device=DEVICE, dtype=torch.double)
-  
-def Rz(theta):
-    return torch.tensor([[ torch.cos(theta), -torch.sin(theta), 0 ],
-                   [ torch.sin(theta), torch.cos(theta) , 0 ],
-                   [ 0           , 0            , 1 ]], device=DEVICE, dtype=torch.double)
--- ProjectName/src/main/__init__.py
+++ ProjectName/src/main/__init__.py
@@ -16,7 +16,7 @@ class MyNet(object):
     """Network Class made of several conv layers followed by RelU activation function"""
     def __init__(self, first_layer, last_layer):
         """
-        This is connection string o connect to sql server
+        This is creating the network model based on parameters
         :param in_channels: int
         :param out_channels: int
         :param kernel_size: int
@@ -40,7 +40,7 @@ class MyNet(object):
 
     def forward(self, x):
         """
-        This method is the output of the network
+        This method will return the output of the network after we passed an input
         :return: torch.tensor
         """
         return self.layers(x)
