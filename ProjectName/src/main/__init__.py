a63b82064b1332b8d5d45124aba91eb3612e7bfc
Amin Fadaeinejad <aminfadaei116@gmail.com>
2022-12-25 12:20:48 -0500

    Update __init__.py

--- ProjectName/src/main/__init__.py
+++ ProjectName/src/main/__init__.py
@@ -1,25 +1,48 @@
 """
-DatabaseSetingsModule
+NeuralNetworkModule
 ----------------------
-This is a Module which has Class for Database and database Settings
+This is a Module which has the network model in it
 """
 
+import numpy as np
+from numpy import save
+import math as m
+import matplotlib.pyplot as plt
+import torch.nn as nn
+import torch.nn.functional as F
+import cv2
 
-class Database2(object):
-
+class MyNet(object):
     """Database Class which is responsile for connecting to databse """
-
-    def __init__(self, connection_string):
+    def __init__(self, first_layer, last_layer):
         """
         This is connection string o connect to sql server
-        :param connection_string: str
+        :param in_channels: int
+        :param out_channels: int
+        :param kernel_size: int
+        :param padding: int
         """
-        self.connection_string = connection_string
+        super(MyNet, self).__init__()
+        self.layers = nn.Sequential(
+            nn.Conv2d(in_channels=first_layer, out_channels=8,kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=8, out_channels=8,kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=8, out_channels=8,kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=8, out_channels=8,kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=8, out_channels=8,kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=8, out_channels=last_layer,kernel_size=3, padding=1),
+        )
+        self.id = nn.Identity()
 
-    def connect2(self) -> True:
+    def forward(self, x):
         """
-        This method is used to connect to database
-        :return: Bool
+        This method is the output of the network
+        :return: torch.tensor
         """
-        print("connected to database")
-        return True
+        return self.layers(x)
+
+  
